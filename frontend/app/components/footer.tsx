20a4b8347388041af2d4a300899349be6e5e8889
FSD115-shashirajt20-byte <shashirajt20@gmail.com>
2026-01-06 12:18:19 +0530

    docs : completed phase-1 architecture and database design

---
+++ frontend/app/components/footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer(){
+    return(
+        <div>
+            <div>
+                Live Stock Hub - Digital Cattle Marketplace
+            </div>
+            <div>
+                www.livestockhub.com
+            </div>
+        </div>
+    )
+}
