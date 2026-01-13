c6a5aa836dd7f2709072b726180a2e307dd4c8e2
FSD115-shashirajt20-byte <shashirajt20@gmail.com>
2026-01-13 23:28:57 +0530

    feat: removed old data models and rebuild new data models

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,39 +1,13 @@
-// import express from "express";
-// import cors from "cors";
-// import dotenv from "dotenv";
-// import authRoutes from "./modules/auth/route.js"
-
-// app.use("/api/auth", authRoutes);
-
-// dotenv.config();
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.get("/", (req, res) => {
-//   res.send("LiveStockHub Backend Running...");
-// });
-
-// app.listen(8000, () => console.log("Server running on 8000"));
-
-
-import express from "express"
+import express from "express";
 import cors from "cors";
-import authRoutes from "./routes/auth.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 
 const app = express();
-app.use(cors({
-  origin : "http://localhost:3000",
-  credentials: true
-}));
-app.use(express.json());
+app.use(cors());
 
-app.use("/auth", authRoutes);
-
-
-app.listen(8000, () => {
-  console.log("Server is running");
-});
-
-
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server is running on ${process.env.PORT}`)
+})
