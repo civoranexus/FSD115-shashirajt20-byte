9f6ce942845fbdf8ee0230deca40b23682473c52
FSD115-shashirajt20-byte <shashirajt20@gmail.com>
2026-01-14 23:00:01 +0530

    feat : implemented signup and singin

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,21 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import router from "./route/auth.js";
 
 dotenv.config();
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin:"http:localhost:3000",
+    credentials: true
+}));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/auth", router);
 
 app.listen(process.env.PORT, ()=>{
     console.log(`Server is running on ${process.env.PORT}`)
---
+++ frontend/app/signin/page.tsx
@@ -0,0 +1,40 @@
+'use client'
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const router = useRouter();
+  async function handleSubmit(e){
+    e.preventDefault();
+    const res = await fetch("/api/auth/signin",{
+        method : "POST",
+        headers : {"Content-Type" : "application/json"},
+        body : JSON.stringify({
+
+            email, 
+            password
+        }),
+        credentials: "include"
+    });
+    const data = await res.json();
+    if(data.success){
+        router.push("/");
+    }else{
+        setError(data.message);
+    }
+    console.log(data);
+    console.log(email, password);
+  };
+  return(
+    <form onSubmit={handleSubmit}>
+      <input value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Email"/>
+      <input value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="Password"/>
+      <button>Login</button>
+      <p>{error}</p>
+    </form>
+  )
+}
